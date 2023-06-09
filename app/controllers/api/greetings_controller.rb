bb1c5693191b501da42f78182be4252b9892fbc3
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-09 19:21:43 +0000

    update greeting controller

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,7 +1,11 @@
 class Api::GreetingsController < ApplicationController
     def random
       greeting = Greeting.order("RANDOM()").first
+    if greeting
       render json: { message: greeting.message }
+    else
+      render json: { error: "No greetings found" }, status: :not_found
+    end
   end
 end
 
