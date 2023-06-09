48dca34ed9b05a8350eb8b865899b6c34a231f76
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-09 19:28:22 +0000

    fix rubocop errors

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,11 +1,10 @@
 class Api::GreetingsController < ApplicationController
-    def random
-      greeting = Greeting.order("RANDOM()").first
+  def random
+    greeting = Greeting.order('RANDOM()').first
     if greeting
       render json: { message: greeting.message }
     else
-      render json: { error: "No greetings found" }, status: :not_found
+      render json: { error: 'No greetings found' }, status: :not_found
     end
   end
 end
-
